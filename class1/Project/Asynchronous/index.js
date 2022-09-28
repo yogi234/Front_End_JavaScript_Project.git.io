8848f37a2da0c2fad929d19087464450448e9d91
yogi234 <109338333+yogi234@users.noreply.github.com>
2022-09-28 19:52:03 +0530

    Order Status Management: Get & Display Order Id Complete

--- class1/Project/Asynchronous/index.js
+++
@@ -1,7 +0,0 @@
-const manageOrder = () => {
-    let orderId = document.getElementById('order-id-input').value
-    document.getElementById('order-no-display').innerText = `Status for Order No. ${orderId}`
-    document.getElementById('order-no-display').style.display = 'block'
-    document.getElementsByClassName('order-status-block')[0].classList.replace('btn-light', 'btn-success')
-    document.getElementById('order-id-input').value=''
-}
